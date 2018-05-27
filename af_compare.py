9088d0404011cafd3a4bbd4959947a91d3016e10
James Anderson <jrsa@jrsa.co>
2018-05-26 22:14:16 -0700

    af_compare is more clean, also parts with different flash sizes that are stored in the same xml file are handled properly. also the big pin info structure now uses namedtuple to make the member/field access cleaner but it is not implemented in the 'user' code yet

--- af_compare.py
+++ af_compare.py
@@ -13,7 +13,8 @@ def main(arg):
 
     chooser = choose.Stm32Chooser(DB_DIRECTORY)
 
-    mcu_pin_lists = {pn : chooser.pinlist_for_part_with_signals(pn) for pn in pns}
+    mcu_pin_lists = {pn : chooser.pindesc_for_part(pn) for pn in pns}
+    assert list(mcu_pin_lists.keys()) == pns
 
     comp1_pn = list(mcu_pin_lists.keys())[0]
     comp2_pn = list(mcu_pin_lists.keys())[1]
@@ -23,71 +24,65 @@ def main(arg):
 
     assert len(comp1_sig_list) == len(comp2_sig_list)
 
+    # storage for results, all are dictionaries indexed by pin number
     signal_intersections = {}
-    identical_pins = []
     comp1_only_signals = {}
     comp2_only_signals = {}
     pin_name_differences = {}
+
+    # except for this array, which holds arbitrary pin numbers
+    identical_pins = []
 
     # iterate the main pin/signal list, keys are pin numbers, k is the current pin
     # number everywhere in this loop
     for k in comp1_sig_list.keys():
 
-        # values in pin list are (pin_name, pin_type, {sig1, sig2, ...})
-        comp1_current_pin = comp1_sig_list[k]
-        comp2_current_pin = comp2_sig_list[k]
+        # values in sig list are (pin_name, pin_type, {sig1, sig2, ...})
 
         # pin name/type differences
-        if comp1_current_pin[0] != comp2_current_pin[0]:
-            pin_name_differences[k] = (comp1_current_pin[0], comp2_current_pin[0])
+        if comp1_sig_list[k][0] != comp2_sig_list[k][0]:
+            pin_name_differences[k] = (comp1_sig_list[k][0], comp2_sig_list[k][0])
 
         # keep some information about singals, if there are any
-        if len(comp2_current_pin[2]) or len(comp1_current_pin[2]):
+        if len(comp2_sig_list[k][2]) or len(comp1_sig_list[k][2]):
 
             # check for intersection of the set of signals available on each pin (of the 2 ICs)
-            signal_intersection = comp1_current_pin[2].intersection(comp2_current_pin[2])
+            signal_intersection = comp1_sig_list[k][2].intersection(comp2_sig_list[k][2])
             if len(signal_intersection):
                 signal_intersections[k] = signal_intersection
 
-            if comp1_current_pin[2] == comp2_current_pin[2]:
+            if comp1_sig_list[k][2] == comp2_sig_list[k][2]:
                 identical_pins.append(k)
 
-            comp1_only = comp1_current_pin[2].difference(comp2_current_pin[2])
+            comp1_only = comp1_sig_list[k][2].difference(comp2_sig_list[k][2])
             if len(comp1_only):
                 comp1_only_signals[k] = comp1_only
-            comp2_only = comp2_current_pin[2].difference(comp1_current_pin[2])
+            comp2_only = comp2_sig_list[k][2].difference(comp1_sig_list[k][2])
             if len(comp2_only):
                 comp2_only_signals[k] = comp2_only
 
     # print out results, again looping over the total amount of pins
     for k in comp1_sig_list.keys():
-        print("pin {} ({})".format(k, comp1_sig_list[k][0]))
+        output_line = str(k) + ' '
         if k in pin_name_differences.keys():
-            print("\tname differs:")
-            print("\t{}: {}".format(comp1_pn, pin_name_differences[k][0]))
-            print("\t{}: {}".format(comp2_pn, pin_name_differences[k][1]))
+            dif = (pin_name_differences[k][0], comp1_pn, pin_name_differences[k][1], comp2_pn)
+            output_line += "({}: {}, {}: {}) (renamed)".format(*dif)
+        else:
+            output_line += "({})".format(comp1_sig_list[k][0])
 
-        if comp1_sig_list[k][1] != "I/O":
-            # these aren't the droids you're looking for
-            continue
+        output_line += '\t'
 
-        if k in identical_pins:
-            print("pin {} has identical signals available on {} and {}".format(k, comp1_pn, comp2_pn))
-            continue
-
-        if k in comp1_only_signals.keys():
-            print("\tonly on {}:".format(comp1_pn))
-            for s in comp1_only_signals[k]:
-                print("\t\t{}".format(s))
         if k in signal_intersections.keys():
-            print("\tcommon: ")
-            for s in signal_intersections[k]:
-                print("\t\t{}".format(s))
-        if k in comp2_only_signals.keys():
-            print("\tonly on {}:".format(comp2_pn))
-            for s in comp2_only_signals[k]:
-                print("\t\t{}".format(s))
+            output_line += " ".join(signal_intersections[k])
+
+        # if k in comp1_only_signals.keys():
+        #     print(comp1_only_signals[k])
+
+        print(output_line)
 
 if __name__ == '__main__':
-    main(sys.argv)
+    try:
+        main(sys.argv)
+    except RuntimeError as e:
+        print(e)
     sys.exit(0)
--- choose/__init__.py
+++ choose/__init__.py
@@ -1,13 +1,31 @@
 import xml.etree.ElementTree
 import re
 import glob
-
-from os.path import exists
+from collections import namedtuple
+import os.path
 
 """
 dependent on https://github.com/esden/stm32cube-database, pass the path to it
 during construction of objects of the class below 
 """
+
+# source: various STM32 datasheets
+PACKAGES = {
+    'T' : 'LQFP',
+    'K' : 'UFBGA',
+    'H' : 'TFBGA',
+    'Y' : 'WLCSP',
+}
+
+Pn = namedtuple('Pn', ['family', 'subtype', 'pincount', 'flashsize', 'package'])
+def split_pn(pn):
+    if pn[5] == '(':
+        closing_paren = pn.index(')')
+        return (pn[:2], pn[2:4], pn[4:5], pn[6:closing_paren].split("-"), pn[closing_paren+1:closing_paren+2] )
+    else:
+        return (pn[:2], pn[2:4], pn[4:5], pn[5:6], pn[6:7])
+
+PinDesc = namedtuple('PinDesc', ['name', 'type', 'signals'])
 
 PIN_LIST_ATTRS = ['Position', 'Name', 'Type']
 
@@ -17,6 +35,9 @@ def pin_tree_to_list(pin_tree_entries):
 def pin_tree_to_list_with_signals(pin_tree_entries):
     return {int(p.attrib['Position']) : (p.attrib['Name'], p.attrib['Type'], set([s.attrib["Name"] for s in p.findall("Signal")])) for p in pin_tree_entries}
 
+def pin_tree_to_descs(pin_tree_entries):
+    return {int(p.attrib['Position']) : PinDesc(p.attrib['Name'], p.attrib['Type'], set([s.attrib["Name"] for s in p.findall("Signal")])) for p in pin_tree_entries}
+
 class Stm32Chooser(object):
     def __init__(self, fn):
         self.database_dir = fn
@@ -25,9 +46,35 @@ class Stm32Chooser(object):
         return sorted(glob.glob(self.database_dir + "/STM32*.xml"))
 
     def filename_for_part(self, part):
+        search_pn = Pn(*split_pn(part))
+
+        if search_pn.package in ['K', 'H', 'Y']:
+            raise RuntimeError("BGA packages not supported yet, asked for package type {} ({})".format(search_pn.package, PACKAGES[search_pn.package]))
+
         path = self.database_dir + "/STM32{}.xml".format(part)
-        if exists(path):
+        if os.path.exists(path):
             return path
+
+        # see if the part is covered by a file with a (a-b) expression in the filename
+
+        part_glob_expr = "".join(["STM32", part[:5], "*.xml"])
+        part_glob = glob.glob(self.database_dir + part_glob_expr)
+
+        if len(part_glob) == 1:
+            pn_string = os.path.basename(part_glob[0])[5:-4]
+            base_pn = Pn(*split_pn(pn_string))
+            if search_pn.flashsize in base_pn.flashsize and search_pn.package == base_pn.package:
+                return part_glob[0]
+
+        else:
+            for g in part_glob:
+                pn_string = os.path.basename(g)[5:-4]
+                base_pn = Pn(*split_pn(pn_string))
+
+                if search_pn.flashsize in base_pn.flashsize and search_pn.package == base_pn.package:
+                    return g
+
+        raise RuntimeError("{} not found dog".format(search_pn))
 
     def tree_for_filename(self, fn):
         data = None
@@ -50,12 +97,22 @@ class Stm32Chooser(object):
 
     def pinlist_for_part(self, part):
         fn = self.filename_for_part(part)
+        print("loading pin info from {}".format(fn))
         mcu_tree = self.tree_for_filename(fn)
         pin_tree = mcu_tree.findall("Pin")
         return pin_tree_to_list(pin_tree)
 
     def pinlist_for_part_with_signals(self, part):
         fn = self.filename_for_part(part)
+        print("loading pin info from {}".format(fn))
         mcu_tree = self.tree_for_filename(fn)
         pin_tree = mcu_tree.findall("Pin")
         return pin_tree_to_list_with_signals(pin_tree)
+
+    def pindesc_for_part(self, part):
+        fn = self.filename_for_part(part)
+        print("loading pin info from {}".format(fn))
+        mcu_tree = self.tree_for_filename(fn)
+        pin_tree = mcu_tree.findall("Pin")
+        return pin_tree_to_descs(pin_tree)
+
