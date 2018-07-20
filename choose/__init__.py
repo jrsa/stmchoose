2a7dfc647163467537d8a34d7bf66a57115ec771
James Anderson <jrsa@jrsa.co>
2018-07-19 21:11:56 -0700

    clean unused stuff and add join_pn()
    join_pn takes a Pn and returns a string, the exact opposite of split_pn

--- choose/__init__.py
+++ choose/__init__.py
@@ -10,15 +10,6 @@ during construction of objects of the class below
 """
 DEFAULT_DATABASE_DIR = '../stm32cube-database/db/mcu/'
 
-
-# source: various STM32 datasheets
-PACKAGES = {
-    'T' : 'LQFP',
-    'K' : 'UFBGA',
-    'H' : 'TFBGA',
-    'Y' : 'WLCSP',
-}
-
 Pn = namedtuple('Pn', ['family', 'subtype', 'pincount', 'flashsize', 'package'])
 def split_pn(pn):
     if pn[5] == '(':
@@ -27,15 +18,13 @@ def split_pn(pn):
     else:
         return (pn[:2], pn[2:4], pn[4:5], pn[5:6], pn[6:7])
 
+def join_pn(pn):
+    try:
+        return "".join(pn)
+    except TypeError:
+        return join_pn(Pn(**{k: pn._asdict()[k] for k in pn._asdict() if k is not 'flashsize'}, flashsize="({})".format("-".join(pn.flashsize))))
+
 PinDesc = namedtuple('PinDesc', ['name', 'type', 'signals'])
-
-PIN_LIST_ATTRS = ['Position', 'Name', 'Type']
-
-def pin_tree_to_list(pin_tree_entries):
-    return {p.attrib["Position"] : [p.attrib["Name"], p.attrib["Type"]] for p in pin_tree_entries}
-
-def pin_tree_to_list_with_signals(pin_tree_entries):
-    return {p.attrib['Position'] : (p.attrib['Name'], p.attrib['Type'], set([s.attrib["Name"] for s in p.findall("Signal")])) for p in pin_tree_entries}
 
 def pin_tree_to_descs(pin_tree_entries):
     return {p.attrib['Position'] : PinDesc(p.attrib['Name'], p.attrib['Type'], set([s.attrib["Name"] for s in p.findall("Signal")])) for p in pin_tree_entries}
@@ -94,24 +83,8 @@ class Stm32Chooser(object):
 
         return tree
 
-    def pinlist_for_part(self, part):
-        fn = self.filename_for_part(part)
-        print("loading pin info from {}".format(fn))
-        mcu_tree = self.tree_for_filename(fn)
-        pin_tree = mcu_tree.findall("Pin")
-        return pin_tree_to_list(pin_tree)
-
-    def pinlist_for_part_with_signals(self, part):
-        fn = self.filename_for_part(part)
-        print("loading pin info from {}".format(fn))
-        mcu_tree = self.tree_for_filename(fn)
-        pin_tree = mcu_tree.findall("Pin")
-        return pin_tree_to_list_with_signals(pin_tree)
-
     def pindesc_for_part(self, part):
         fn = self.filename_for_part(part)
-        print("loading pin info from {}".format(fn))
         mcu_tree = self.tree_for_filename(fn)
         pin_tree = mcu_tree.findall("Pin")
         return pin_tree_to_descs(pin_tree)
-
