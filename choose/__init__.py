5e3f8e04cdcd16779e91f04331ee40c2354e4971
James Anderson <jrsa@jrsa.co>
2018-08-04 07:50:04 -0700

    document split_pn and join_pn

--- choose/__init__.py
+++ choose/__init__.py
@@ -17,6 +17,14 @@ PinDesc = namedtuple('PinDesc', ['name', 'type', 'signals'])
 
 
 def split_pn(pn):
+    """
+    return a tuple containing part number components. from a string of the
+    form 'F469NIHx', return ('F4', '69', 'N', 'I', 'H')
+
+    the branch handles strings of the form 'F103C(8-B)Tx' where '8' and 'B'
+    are distinct values for the 'flashsize' field on Pn. a substring of the
+    form '(A-B)' results in a list containing 'A' and 'B'
+    """
     if pn[5] == '(':
         closing_paren = pn.index(')')
         return (pn[:2], pn[2:4], pn[4:5], pn[6:closing_paren].split("-"),
@@ -25,6 +33,10 @@ def split_pn(pn):
         return (pn[:2], pn[2:4], pn[4:5], pn[5:6], pn[6:7])
 
 def join_pn(pn):
+    """
+    return a part number string of the form 'F469NIHx', given an instance
+    of Pn
+    """
     try:
         return "".join(pn)
     except TypeError:
